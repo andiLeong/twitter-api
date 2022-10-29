1422e0e229ed3da052329a9168f73f8cb41ae6e4
andiLeong <alwayssmile20150724@gmail.com>
2022-10-29 17:01:10 +0800

    first commit

---
+++ database/factories/TweetFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Arr;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Tweet>
+ */
+class TweetFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        $day = Arr::random(range(1,10));
+        return [
+            'user_id' => User::factory(),
+            'body' => $this->faker->sentence(10),
+            'created_at' => now()->subDays($day),
+        ];
+    }
+}
