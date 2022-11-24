484ab779a1be9a8027ea7d1f3f34713585d04973
andiLeong <alwayssmile20150724@gmail.com>
2022-11-24 13:38:44 +0800

    fix cant delete user

--- database/migrations/2022_10_29_075227_create_tweets_table.php
+++ database/migrations/2022_10_29_075227_create_tweets_table.php
@@ -14,7 +14,7 @@ return new class extends Migration {
     {
         Schema::create('tweets', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->unsignedBigInteger('retweeted_id')->nullable();
             $table->string('body')->nullable();
             $table->timestamps();
